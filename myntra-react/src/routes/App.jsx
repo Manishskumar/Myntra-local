a6e77c45211e0b0626beb3ca59c56a11a675361f
Manish_kumar <manishbbu0202@gmail.com>
2024-09-18 18:20:52 +0530

    Myntra_clone

---
+++ myntra-react/src/routes/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import { Outlet } from 'react-router-dom';
+import Fetchitems from '../components/Fetchitems';
+import { useSelector } from 'react-redux';
+import LoadingSpinner from '../components/LoadingSpinner';
+
+const App = () => { 
+
+  const fetchStatus= useSelector((store) => store.fetchStatus);
+
+  return (
+    <div>
+      <Header/>
+      <Fetchitems />
+     {fetchStatus.currentlyFetching ? <LoadingSpinner /> :  <Outlet />} 
+    <Footer/>
+    </div>
+  )
+};
+
+export default App;
